1019625db129b45426d10f8f10d7cf8634921f6a
Justin Tan <jtan7th@gmail.com>
2024-09-10 08:56:18 +1200

    fully styled the modal

--- app/controllers/modal_controller.js
+++ app/controllers/modal_controller.js
@@ -1,19 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["frame"]
+  // static targets = ["frame"]
 
-  open(event) {
-    event.preventDefault()
-    const url = event.currentTarget.getAttribute("href")
-    const frame = document.getElementById("modal")
-    frame.src = url
-    frame.style.display = "block"
+  // open(event) {
+  //   event.preventDefault()
+  //   const url = event.currentTarget.getAttribute("href")
+  //   const frame = document.getElementById("modal")
+  //   frame.src = url
+  //   frame.style.display = "block"
+  // }
+
+  // close() {
+  //   const frame = document.getElementById("modal")
+  //   frame.style.display = "none"
+  //   frame.src = ""
+  // }
+
+  static targets = ["panel"]
+
+  connect() {
+    setTimeout(() => this.showModal(), 50)
   }
 
-  close() {
-    const frame = document.getElementById("modal")
-    frame.style.display = "none"
-    frame.src = ""
+  hideModal() {
+    this.panelTarget.classList.remove("opacity-100", "translate-y-0", "sm:scale-100")
+    this.panelTarget.classList.add("opacity-0", "translate-y-4", "sm:translate-y-0", "sm:scale-95")
+  }
+
+  showModal() {
+    this.panelTarget.classList.remove("opacity-0", "translate-y-4", "sm:translate-y-0", "sm:scale-95")
+    this.panelTarget.classList.add("opacity-100", "translate-y-0", "sm:scale-100")
   }
 }
