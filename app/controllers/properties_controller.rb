29908f9f14640a11ab27172302d622b6196f90b6
Justin Tan <jtan7th@gmail.com>
2024-09-10 09:10:28 +1200

    fixed the 'content missing' frame issue when returning from a turbo_frame

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,12 @@
 class PropertiesController < ApplicationController
   def index
-    @properties = Property.all.order(:suburb, :address)
+    @properties = Property.all  # or whatever scope you're using
+
+    if turbo_frame_request?
+      render :index, layout: false
+    else
+      render :index
+    end
   end
 
   def show
