9924830cb61ac7e4810ba29d859f274bd128c70e
Justin Tan <jtan7th@gmail.com>
2024-09-11 07:44:21 +1200

    centered the title and the subtitle

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "./controllers/application"
+import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-
+import Turbo from "@hotwired/turbo"
 eagerLoadControllersFrom("controllers", application)
