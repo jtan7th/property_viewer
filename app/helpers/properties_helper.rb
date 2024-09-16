9dbfc1006184dc155e32e2ee0607c053ce1f2277
Justin Tan <jtan7th@gmail.com>
2024-09-16 16:46:55 +1200

    added a properties_helper.rb to assign pill colors based on suburb

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -1,2 +1,17 @@
 module PropertiesHelper
-end
+    def suburb_pill(suburb)
+      suburb = suburb.strip.downcase
+      case suburb
+      when "corsair bay"
+        pill_class = "bg-green-100 text-green-700"
+      when "lyttelton"
+        pill_class = "bg-green-100 text-green-700"
+      when "cass bay"
+        pill_class = "bg-blue-100 text-blue-700"
+      else
+        return suburb.titleize # Return regular text for non-special suburbs
+      end
+  
+      content_tag :span, suburb.titleize, class: "inline-flex items-center rounded-full #{pill_class} px-2 py-1 text-sm font-medium"
+    end
+  end
