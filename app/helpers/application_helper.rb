0b13dd27ad0deb1fb4d2babe976b33848a30fb99
Justin Tan <jtan7th@gmail.com>
2024-09-19 08:15:06 +1200

    make sale price more visually pleasing with  $ signs and applying 'k' and 'm' isntead of many zeroes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def format_price(price)
+    return 'N/A' if price.nil?
+
+    case price.to_s.length
+    when 1..5
+      number_to_currency(price)
+    when 6
+      "$#{price / 1000}k"
+    when 7
+      "$#{(price / 1000000.0).round(3)}m"
+    else
+      number_to_currency(price)
+    end
+  end
 end
