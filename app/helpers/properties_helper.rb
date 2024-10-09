3a05f99f0f550f5e5878ea249ca63cfa124a3848
Justin Tan <jtan7th@gmail.com>
2024-10-10 09:21:13 +1300

    made colors consistent across the app

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -3,16 +3,16 @@ module PropertiesHelper
       suburb = suburb.strip.downcase
       case suburb
       when "corsair bay"
-        pill_class = "bg-green-100 text-green-700"
+        pill_class = "bg-green-50 text-green-600"
       when "lyttelton"
-        pill_class = "bg-green-100 text-green-700"
+        pill_class = "bg-green-50 text-green-600"
       when "cass bay"
-        pill_class = "bg-blue-100 text-blue-700"
+        pill_class = "bg-blue-50 text-blue-600"
       else
         return suburb.titleize # Return regular text for non-special suburbs
       end
   
-      content_tag :span, suburb.titleize, class: "inline-flex items-center rounded-full #{pill_class} px-2 py-1 text-sm font-medium"
+      content_tag :span, suburb.titleize, class: "inline-flex items-center rounded-full #{pill_class} px-2 py-1 text-sm font-medium bg-opacity-50"
     end
 
     def suburb_options
