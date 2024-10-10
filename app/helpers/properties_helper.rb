0e25f71e4ee6a40252ad60bd3ee637a48c18d1e1
Justin Tan <jtan7th@gmail.com>
2024-10-11 09:30:49 +1300

    added pill colors to the other suburbs

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -3,11 +3,23 @@ module PropertiesHelper
       suburb = suburb.strip.downcase
       case suburb
       when "corsair bay"
-        pill_class = "bg-green-50 text-green-600"
-      when "lyttelton"
-        pill_class = "bg-green-50 text-green-600"
+        pill_class = "bg-purple-50 text-purple-600"
       when "cass bay"
+        pill_class = "bg-fuchsia-50 text-fuchsia-600"
+      when "redcliffs"
+        pill_class = "bg-red-50 text-red-600"
+      when "clifton"
+        pill_class = "bg-amber-50 text-amber-600"
+      when "richmond hill"
+        pill_class = "bg-green-50 text-green-600"
+      when "moncks bay"
+        pill_class = "bg-lime-50 text-lime-600"
+      when "sumner"
         pill_class = "bg-blue-50 text-blue-600"
+      when "mount pleasant"
+        pill_class = "bg-cyan-50 text-cyan-600"
+      when "scarborough"
+        pill_class = "bg-orange-50 text-orange-600"
       else
         return suburb.titleize # Return regular text for non-special suburbs
       end
