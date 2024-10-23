6baaa442ce1c139658d128e23911033c3ac27adb
Justin Tan <jtan7th@gmail.com>
2024-10-24 10:43:23 +1300

    made scraper run headlessly

--- app/services/url_scraper_service.rb
+++ app/services/url_scraper_service.rb
@@ -10,7 +10,7 @@ class UrlScraperService
         options = Selenium::WebDriver::Chrome::Options.new
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
       
         driver = Selenium::WebDriver.for :chrome, options: options
         wait = Selenium::WebDriver::Wait.new(timeout: 15)
