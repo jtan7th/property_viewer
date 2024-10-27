91d43276fd338538f68eca57bdfe5a36cc4dc261
Justin Tan <jtan7th@gmail.com>
2024-10-28 10:50:05 +1300

    added a upwards and downward facing triangle for sorting

--- app/helpers/toggle_direction_helper.rb
+++ app/helpers/toggle_direction_helper.rb
@@ -4,4 +4,9 @@ module ToggleDirectionHelper
     next_direction = params[:sort_by] == column ? current_direction : 'asc'
     next_direction
   end
+
+  def sort_indicator(column)
+    return unless params[:sort_by] == column
+    params[:direction] == 'asc' ? '▲' : '▼'
+  end
 end
